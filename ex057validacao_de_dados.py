dcbf117e19c6bbb9c1a6b8c2cd2c5262c9ea611c
diego2082 <diego.oliveira2082@gmail.com>
2023-10-01 14:55:23 -0300

    validar dados

---
+++ ex057validacao_de_dados.py
@@ -0,0 +1,11 @@
+
+sexo = str(input('Informe sexo [F/M]: ')).upper().strip()[0]
+while sexo not in 'FfMm':
+        sexo=str(input('Dados invalidos! Digitar F ou M: '))
+print('Sexo informado corretamente.')
+
+
+
+
+
+
